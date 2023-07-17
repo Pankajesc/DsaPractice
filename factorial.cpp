b6d3dec76a2f8b1373b2e4cadd2920fdf745f158
Pankaj Kumar Yadav <125720698+Pankaj8287@users.noreply.github.com>
2023-07-17 21:58:31 +0530

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    int fact = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    return fact;
+}
+
+int nCr(int n, int r)
+{
+
+    int num = factorial(n);
+    int denom = factorial(r) * factorial(n - r);
+    int answer = num / denom;
+
+    return answer;
+}
+
+int main()
+{      
+     int n, r;
+     cin>>n;
+     cin>>r;
+        cout<<nCr(n,r);
+    return 0;
+}
