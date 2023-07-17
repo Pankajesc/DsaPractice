3cc57bbcc5e76b11dcb85567c9dfb4f77f18a5be
Pankaj Kumar Yadav <125720698+Pankaj8287@users.noreply.github.com>
2023-07-17 21:59:05 +0530

    Create Bitwise.cpp

---
+++ Bitwise.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+   int main(){
+
+
+        int a=4;
+        int b=6;
+
+        cout<<"a&b "<<(a & b)<<endl;
+         cout<<"a&b "<<(a | b)<<endl;
+          cout<<"a&b "<<(a ^ b)<<endl;
+           cout<<"a&b "<<(~a)<<endl;
+
+
+
+
+    return 0;
+   }
